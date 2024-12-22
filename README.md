e1c67ec128957607d550b3ac6c235f556d2ffe86
Neil Birkbeck <birkbeck@google.com>
2024-12-21 19:45:01 -0800

    Answering the first question.

---
+++ README.md
@@ -0,0 +1,3 @@
+Questions to answer:
+
+How much farther does making the drop 2x bigger take you?
--- simulate.m
+++ simulate.m
@@ -1,6 +1,9 @@
-function simulate(height, drop_height, jump_length, hill_angle)
+function [pos, v, all_pos] = simulate(height, drop_height, jump_length, hill_angle, draw)
   slope_dx =  cos(hill_angle);
   slope_dy = -sin(hill_angle);
+  if nargin < 5
+    draw = 1;
+  end
 
 % Set up the initial position so that (0, 0) is where ramp meets ground.
   pos = [height / slope_dy * slope_dx - jump_length, height + drop_height];
@@ -9,14 +12,19 @@ function simulate(height, drop_height, jump_length, hill_angle)
   dt = 0.025;
   t = 0;
 
+  all_pos = [];
   while pos(2) > 0,
-    clf;
-    plot([opos(1), -jump_length, 0], [opos(2), drop_height, drop_height], 'r'); hold on;
-    plot([0], [0], 'r*');
-    plot([pos(1)], [pos(2)], 'b*');
-    axis equal;
-    drawnow;
-    pause(0.025);
+    last_pos = pos;
+    all_pos = [all_pos; pos];
+    if draw,
+      clf;
+      plot([opos(1), -jump_length, 0], [opos(2), drop_height, drop_height], 'r'); hold on;
+      plot([0], [0], 'r*');
+      plot([pos(1)], [pos(2)], 'b*');
+      axis equal;
+      drawnow;
+      pause(0.025);
+    end
     if pos(1) < -jump_length,
       a = [slope_dx, slope_dy] * slope_dy * -9.81;
     elseif pos(1) < 0,
@@ -34,11 +42,12 @@ function simulate(height, drop_height, jump_length, hill_angle)
     if pos(1) > -jump_length && pos(1) < 0,
       pos(2) = max(pos(2), drop_height);
     end
-    if !x_gt_0 && pos(1) >= 0,
+    if !x_gt_0 && pos(1) >= 0 && nargout == 0,
       sprintf('Transition:')
       v, norm(v), t
     end
     t += dt;
   end
 
-  pos, norm(v)
+  ratio = (pos(2)) / (pos(2) - last_pos(2));
+  pos -= v * ratio * dt;
